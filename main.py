4564dde7f9653a13c524aaf408b7cb198bb267cb
Georges Briche <gbriche59@yahoo.fr>
2023-08-21 17:28:10 +0200

    Extraction des données d'une page Produit

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = " http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-page = requests.get(url)
-# Voir le code html source
-print(page.content)
